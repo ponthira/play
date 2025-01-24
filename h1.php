97964bd5ea7dde9c7a045a33c48d16b7b986913e
ponthira <24511@srithatpit.ac.th>
2025-01-24 11:01:14 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+    <p>-----การแสดงข้อความแบบหัวข้อ-----</p>
+    <div>
+            <h1>HTMLเบื้องต้น(h1)</h1>
+    </div>
+    <div>
+            <h2>HTMLเบื้องต้น(h2)</h2>
+            <h3>HTMLเบื้องต้น(h3)</h3>
+            <h4>HTMLเบื้องต้น(h4)</h4>
+            <h5>HTMLเบื้องต้น(h5)</h5>
+            <h6>HTMLเบื้องต้น(h6)</h6>
+    </div>
+    <p>-----การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ-----</p>
+    
+    <div>
+            <b>พรธีรา คงการเรียน</b><br>
+            <i>พรธีรา คงการเรียน</i><br>
+            <u>พรธีรา คงการเรียน</u><br>
+    </div>
+            <p><p>-----การ link-----</p></p>
+    <div>
+        <!--linkหน้าเดียว-->
+        <a href = "https://www.google.co.th">Google</a><br>
+    </div>
+
+</body>
+</html>
